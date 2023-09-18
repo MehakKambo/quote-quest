59cdbd01bd2d5c354fe9447bdaac506066be553b
MehakKambo <mehakpreetp12@gmail.com>
2023-09-18 16:48:58 -0700

    Added quoteByID component

--- quotes-app/src/App.js
+++ quotes-app/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
 import RandomQuote  from './components/RandomQuote';
+import QuoteByID from './components/QuoteByID';
 
 function App() {
   return (
     <main>
       <Router>
         <Routes>
+          <Route path="/" exact element={<Home/>} />
           <Route path="/quotes/random" exact element={<RandomQuote/>} />
+          <Route path="/quotes/quoteID" exact element={<QuoteByID/>} />
         </Routes>
       </Router>
     </main>
---
+++ quotes-app/src/components/QuoteByID.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import '../styles/QuoteByID.css';
+
+function QuoteByID() {
+  const [quote, setQuote] = useState(null);
+  const [quoteId, setQuoteId] = useState(1); // Default to quote ID 1
+  const [isFetching, setIsFetching] = useState(false);
+  const [notFound, setNotFound] = useState(false); // State to track 404 response
+
+  const fetchQuoteById = async () => {
+    setIsFetching(true);
+
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/quotes/${quoteId}`);
+      const data = await response.json();
+
+      if (response.ok) {
+        setNotFound(false); // Reset notFound state
+        setQuote(data);
+      } else if (response.status === 404) {
+        setQuote(null);
+        setNotFound(true); // Set notFound state to true
+      } else {
+        setQuote(null);
+        setNotFound(false);
+        console.error('Error fetching quote:', data.error);
+      }
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  return (
+    <div className="QuoteByID">
+      <h1>Quote by ID</h1>
+      <div id="quote-id-input">
+        <label htmlFor="quoteId">Enter Quote ID: </label>
+        <input
+          type="number"
+          id="quoteId"
+          value={quoteId}
+          onChange={(e) => setQuoteId(e.target.value)}
+        />
+        <br></br><br></br>
+        <button onClick={fetchQuoteById} disabled={isFetching}>
+          {isFetching ? 'Fetching Quote...' : 'Get Quote'}
+        </button>
+      </div>
+      <div id="quote-container">
+        {notFound ? (
+          <p>No quote found with the specified ID.</p>
+        ) : (
+          quote !== null && (
+            <div>
+              <p><strong>Quote:</strong> {quote.quote}</p>
+              <p><strong>Author:</strong> {quote.author}</p>
+              <p><strong>Category:</strong> {quote.category}</p>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default QuoteByID;
